57c1a06f76266a3641a67fcf94145f087d658327
BernisM <hellobernis@outlook.com>
2023-01-08 22:07:53 +0100

    test test

--- Introduction.py
+++ Introduction.py
@@ -1,2 +1,12 @@
-import cv2
-import numpy as np
+import numpy as np 
+from numpy import genfromtxt
+
+fld = r'C:\Users\massw\OneDrive\Bureau\Programmation\Python_R\Computer-Vision-with-Python\DATA'
+file = r"bank_note_data.txt"
+path = '{}\{}'.format(fld,file)
+
+data = genfromtxt(path,delimiter=',')
+
+data
+
+labels = data[:,4]
