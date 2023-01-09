5203174a3b4efd6f383c09401632f41fe21db50a
BernisM <hellobernis@outlook.com>
2023-01-09 10:06:25 +0100

    Finished

--- Introduction.py
+++ Introduction.py
@@ -27,6 +27,7 @@ scaler_object = MinMaxScaler()
 scaler_object.fit(x_train)
 
 scaled_x_train = scaler_object.transform(x_train)
+# scaled_x_test --> New data que le modele n'a jamais vu
 scaled_x_test = scaler_object.transform(x_test)
 
 scaled_x_test.min()
@@ -34,12 +35,40 @@ scaled_x_test.min()
 from keras.models import Sequential
 from keras.layers import Dense
 
+# Creates model
 model = Sequential()
+# 8 Neurons, expects input of 4 features. 
+# 4 neurones, 4 dimensions, activation = ReLU
 model.add(Dense(4,input_dim=4,activation='relu'))
-model.add(Dense())
+# Add another Densely Connected layer (every neuron connected to every neuron in the next layer)
+model.add(Dense(8,activation='relu'))
+# Last layer simple sigmoid function to output 0 or 1 (our label)
+model.add(Dense(1,activation='sigmoid'))
 
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 
+# Train/Fit the model
+# 1 epochs --> Parcourir données d'entraitements une fois
+model.fit(scaled_x_train,y_train,epochs=50,verbose=2)
 
+model.metrics_names
+from sklearn.metrics import confusion_matrix,classification_report
+
+predictions = np.argmax(model.predict(scaled_x_test),axis=-1)
+
+confusion_matrix(y_test,predictions)
+
+print(classification_report(y_test,predictions))
+
+## Saving and Loading Models
+
+model.save('mymediumodel.h5')
+
+from keras.models import load_model
+
+new_model = load_model('mymediumodel.h5')
+
+new_model.predict(scaled_x_test)
 
 
 
